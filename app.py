2fd3a25ac46dc16e03cc2e65faf7569eff721114
ALBERTO MILET <betomilet@hotmail.com>
2025-06-12 09:39:43 -0300

    Deploy inicial

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='.')
 
 @app.route('/')
 def home():
@@ -14,9 +14,14 @@ def termos():
 def privacidade():
     return send_from_directory('.', 'privacidade.html')
 
-@app.route('/logo.png')
+@app.route('/logo.jpg')
 def logo():
-    return send_from_directory('.', 'logo.png')
+    return send_from_directory('.', 'logo.jpg')
+
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory('.', filename)
 
 if __name__ == '__main__':
     app.run()
+
